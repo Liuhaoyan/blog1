50817ab6c48497522c2eed0deeb113fab1302f67
Liuhaoyan <854215903@qq.com>
2016-09-29 00:12:47 +0800

    显示界面

---
+++ application/views/index.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <base href="<?php echo site_url();?>">
+    <script>
+        window.addEventListener("load",function(){
+            setTimeout(hideURLbar,0);
+
+        },false);
+        function hideURLbar(){
+            window.scrollTo(0,1);
+        }
+    </script>
+</head>
+<body>
+<div class ="sidebar-menu">
+    <div class="top-section">
+        <div class="profile-image">
+            <img src="img/7.jpg">
+        </div>
+        <h3 class="profile-title">Gloria 柳昊妍</h3>
+        <div class="total-location">
+            <span class="location-icon"></span>
+            <span class="school">黑龙江大学</span>
+        </div>
+    </div>
+    <div class="main-navigation">
+        <ul class="navigation">
+            <li>
+                <a href="#top" class="active"><i class="fa fa-globe"></i>welcome</a>
+            </li>
+            <li>
+                <a href="#skill"><i class="fa fa-pencil"></i>skills</a>
+            </li>
+            <li>
+                <a href="#project"><i class="fa fa-paperclip"></i>projects</a>
+            </li>
+            <li>
+                <a href="#title"><i class="fa fa-link"></i>titles</a>
+            </li>
+            <li>
+                <a href="#contact"><i class="fa fa-link"></i>contact me</a>
+            </li>
+        </ul>
+    </div>
+    <div class="social-icons">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-google-plus"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-youtube"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-rss"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="banner-bg" id="top">
+    <div class="banner-overlay"></div>
+    <div class="welcome-text">
+        <h2>WELCOME MY WEBSITE</h2>
+        <h5>IF YOU HAVE DREAMS<br>
+            YOU MUST TRY YOUR BEST TO COMPLETE IT</h5>
+    </div>
+</div>
+
+<div class="main-content">
+    <div class="fluid-container">
+        <div class="content-wrapper">
+
+            <!-- skills -->
+            <div class=" skills-content" id="skills">
+                <div class="text-stitle">
+                    <div>skills</div>
+                </div>
+                <span class="learn-skills">JS:</span>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" ariavaluemax="100" style="width: 80%"></div>
+                    <span class="sr-only">80% Complete</span>
+                </div>
+
+                <span class="learn-skills">JQuery:</span>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" ariavaluemax="100" style="width: 70%"></div>
+                    <span class="sr-only">70% Complete</span>
+                </div>
+
+
+                <span class="learn-skills">PHP:</span>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" ariavaluemax="100" style="width:55%"></div>
+                    <span class="sr-only">55% Complete</span>
+                </div>
+
+
+                <span class="learn-skills">NodeJs:</span>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" ariavaluemax="100" style="width: 60%"></div>
+                    <span class="sr-only">60% Complete</span>
+                </div>
+
+
+                <span class="learn-skills">Angular:</span>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" ariavaluemax="100" style="width: 45%"></div>
+                    <span class="sr-only">45% Complete</span>
+                </div>
+            </div>
+            <hr>
+            <!--skills end-->
+
+            <!-- project -->
+            <div class="page-section" id="projects">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="widget-title">PHOTOS OF WHAT I DO</h4>
+                        <p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Maecenas et lorem molestie, maximus justo dignissim, cursus nisl. Nullam at ante quis ex pharetra pulvinar quis id dolor. Integer lorem odio, euismod ut sem sit amet, imperdiet condimentum diam.</p>
+                    </div>
+                </div>
+                <div class="row projects-holder">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/1.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5><a href="#">Pellentesque porta ligula</a></h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/2.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5>
+                                        <a href="#">Pellentesque porta ligula</a>
+                                    </h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus</
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/3.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5>
+                                        <a href="#">Pellentesque porta ligula</a>
+                                    </h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus</
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/4.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5>
+                                        <a href="#">Pellentesque porta ligula</a>
+                                    </h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus</
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/5.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5>
+                                        <a href="#">Pellentesque porta ligula</a>
+                                    </h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus</
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="project-item">
+                            <img src="img/6.jpg" alt="">
+                            <div class="project-hover">
+                                <div class="inside">
+                                    <h5>
+                                        <a href="#">Pellentesque porta ligula</a>
+                                    </h5>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus</
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <!-- projects ends -->
+
+                <!--  titles -->
+
+                <!--  titles ends-->
+
+                <!--  contact me -->
+                <div class="page-section" id="contact">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h4 class="widget-title">PLACE TO TALK WITH ME</h4>
+                            <p>Vestibulum ac iaculis erat, in semper dolor. Maecenas et lorem molestie, maximus justo dignissim, cursus nisl. Nullam at ante quis ex pharetra pulvinar quis id dolor. Integer lorem odio, euismod ut sem sit amet, imperdiet condimentum diam.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <form action="#" method="post" class="contact-form">
+                            <fieldset class="col-md-4 col-sm-6">
+                                <input type="text" id="your-name" placeholder="Your Name...">
+                            </fieldset>
+                            <fieldset class="col-md-4 col-sn-6">
+                                <input type="text" id="email" placeholder="Your Email...">
+                            </fieldset>
+                            <fieldset class="col-md-4 col-sn-12">
+                                <input type="text" id="your-subject" placeholder="Subject..."></input>
+                            </fieldset>
+                            <fieldset class="col-md-12 col-sm-12">
+                                <textarea name="message" id="message" rows="6" placeholder="Leave your message..."></textarea>
+                            </fieldset>
+                            <fieldset class="col-md-12 col-sm-12">
+                                <input type="submit" class="button big default" value="Send Message">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+                <hr>
+                <!--  contact me ends-->
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div></div>
+    </div>
+    <script src="js/jquery-1.12.4.min.js"></script>
+<script>
+    $(function(){
+
+    });
+</script>
+
+</body>
+</html>
